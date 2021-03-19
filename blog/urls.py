9bf65ba472ca1b19173facc77b3524792d5d6f91
panzhaohua <269258169@qq.com>
2021-03-19 17:17:58 +0800

    增加了html文件

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+# @Time    : 2021/3/19 16:54
+# @Author  : MosesPan
+# @Email   : 269258169@qq.com
+# @File    : urls.py
+# @Software: PyCharm
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
